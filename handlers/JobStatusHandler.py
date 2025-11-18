6b55ffff3ce6b681b30e230f558e856375a6d046
전강현 <aniren@debian.moe>
2025-11-18 15:45:29 +0900

    Update JobStatusHandler.py

--- handlers/JobStatusHandler.py
+++ handlers/JobStatusHandler.py
@@ -9,6 +9,7 @@ class handler(tornado.web.RequestHandler):
     def get(self):
         job_id = self.get_argument("id", None)
         if not job_id or job_id not in config.job_status_map:
-            self.set_status(404); self.write({"error": "job_id not found"})
-            return
-        self.write(config.job_status_map[job_id])
+            self.set_status(404); self.write({"error": "job_id not found"}); return
+
+        res = config.job_status_map.pop(job_id) if config.job_status_map[job_id]["status"] == "done" or config.job_status_map[job_id]["progress"] == 100 else config.job_status_map[job_id]
+        self.write(res)
